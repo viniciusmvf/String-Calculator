eb3750f59b790fdeaf87f6c80c6c02a7807c8276
Andy-Nkumane <nkumaneandy@gmail.com>
2019-06-10 09:12:16 +0200

    Task complete

--- TDD_String_Calculator.py
+++ TDD_String_Calculator.py
@@ -1,21 +1,38 @@
 import re
-# import pytest
 
 def sum_func(num_list):
 	sum =0 
 	for i in num_list:
-		if int(i) < 0:
-			raise ValueError('Negatives not allowed')
-		sum += int(i)
+		if i == '':
+			num = 0
+		else:
+			num = int(i)
+			if num < 0:
+				raise ValueError('Negatives not allowed')
+			elif num > 1000:
+				num = 0
+		sum += num
 	return sum
 
 def add(num):
-	search = re.search('^//(.*)', num)
+	search1 = re.search('^//(.*)', num)
 	if num == '':
 		num_list = []
-	elif search:
-		delimiter = search.groups()[0]
-		num_list =  num[3 + len(delimiter):].split(delimiter)
+	elif '[' in num:
+		cnt = 0
+		for i in num:
+			if i == '[':
+				cnt += 1
+		delim = '\[(.*)]' * cnt
+		search = re.search(f'^//{delim}+', num)
+		if search:
+			delimiter = ''
+			for i in range(cnt):
+				delimiter += search.groups()[i]
+			num_list =  re.split('['+delimiter+'\n\]\[]', num[4 + len(delimiter):])
+	elif search1:
+		delimiter = search1.groups()[0]
+		num_list = re.split('['+delimiter+'\n]', num[3 + len(delimiter):])
 	else:
 		num_list = re.split('[,\n]', num)
 	return sum_func(num_list)
--- test_TDD_String_Calculator.py
+++ test_TDD_String_Calculator.py
@@ -41,3 +41,40 @@ def test_add_negative_exception():
 	with pytest.raises(ValueError):
 		result = add('//;\n-1;2')
 
+def test_add_greater_than_100():
+	result = add('2,1001')
+	assert result == 2
+	result = add('2,1451,5')
+	assert result == 7
+	result = add('2\n1001,4521,6,12')
+	assert result == 20
+
+def test_add_any_length_delimiter():
+	result = add('//[***]\n1***2***3')
+	assert result == 6
+	result = add('//[***]\n14***21***37')
+	assert result == 72
+	result = add('//[***]\n5***2***3')
+	assert result == 10
+	result = add('//[***]\n45***23***1002')
+	assert result == 68
+
+def test_add_multiple_delimiters():
+	result = add('//[*][%]\n1*2%3')
+	assert result == 6
+	result = add('//[*][%][=]\n1*2%3=4')
+	assert result == 10
+	result = add('//[*][%][=][&]\n1*2%3=4&5')
+	assert result == 15
+	result = add('//[*][%][=][&][$]\n1*2%3=4&5$6')
+	assert result == 21
+
+def test_add_any_char_length_delimiter():
+	result = add('//[***][%]\n1***2%3')
+	assert result == 6
+	result = add('//[*][%^%^%^][==]\n1*2%^%^%^3==4')
+	assert result == 10
+	result = add('//[*][%][=][&****^]\n1*2%3=4&****^5')
+	assert result == 15
+	result = add('//[*][%%][===][&&&&][$$$$$]\n1*2%%3===4&&&&5$$$$$6')
+	assert result == 21
